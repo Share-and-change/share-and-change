9917b98706c53ccd09e70b78d18be8877a13f0d6
Arnaud Levy <contact@arnaudlevy.com>
2023-04-14 09:42:20 +0200

    fix

--- db/migrate/20230413174717_drop_kits.rb
+++
@@ -1,5 +0,0 @@
-class DropKits < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :kits
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -100,35 +100,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_13_214811) do
     t.index ["category_id"], name: "index_kits_on_category_id"
   end
 
-  create_table "models", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_models_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
-  end
-
   create_table "needs", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
